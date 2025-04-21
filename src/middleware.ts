200a9a4ad0720d1398c0c63c045474c4648ff3d1
Hadi <969hadiahmed@gmail.com>
2025-04-21 16:16:26 +0530

    fix: redirect to sign-in

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,12 @@ export async function middleware(request: NextRequest) {
         url.pathname.startsWith("/sign-up") ||
         url.pathname.startsWith("/verify") ||
         url.pathname.startsWith("/")
-    ))
-        return NextResponse.redirect(new URL('/dashboard', request.url));
+    )) return NextResponse.redirect(new URL('/dashboard', request.url));
+
+    if (!token && url.pathname.startsWith('/dashboard'))
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+
+    return NextResponse.next();
 }
 
 export const config = {
