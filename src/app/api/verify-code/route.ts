053830e80da4294f3776031be80b5897fada3617
Hadi <969hadiahmed@gmail.com>
2025-04-22 08:53:13 +0530

    feat: route to check verificaton code and verifyuser

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,40 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const { username, code } = await request.json();
+        const user = await UserModel.findOne({ username });
+        if (!user) return Response.json({
+            success: false,
+            message: "User not found"
+        }, { status: 404 });
+
+        const codeValid: boolean = user?.verificationCode === code;
+        const codeNotExpire: boolean = new Date(user.verificationCodeExpiry) > new Date();
+
+        if (codeValid && codeNotExpire) {
+            user.verificationCode = "";
+            user.isVerified = true;
+            await user.save();
+            return Response.json({
+                success: true,
+                message: "User verified successfully"
+            }, { status: 200 });
+        }
+
+        return Response.json({
+            success: false,
+            message: "Invalid otp"
+        }, { status: 401 });
+
+    } catch (error) {
+        console.log("Error while verifying code:", error);
+        return Response.json({
+            success: false,
+            message: "Invalid username"
+        }, { status: 500 })
+    }
+}
