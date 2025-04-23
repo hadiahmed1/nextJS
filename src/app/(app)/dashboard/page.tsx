2e1de1da7e63c021a91fe42edcb5a837cf721fd0
Hadi <969hadiahmed@gmail.com>
2025-04-23 14:20:50 +0530

    feat: getting messages

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,24 @@
+'use client'
+import { useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/apiResponse";
+import { toast } from "sonner";
 
 const page = () => {
-    return(
+
+    useEffect(() => {
+        const getMessages = async () => {
+            try {
+                const response = await axios.get(`/api/get-messages`);
+                console.log("Message:>>",response.data);
+            } catch (error) {
+                const axiosError = error as AxiosError<ApiResponse>;
+                toast.error(axiosError.response?.data.message);
+            }
+        }
+        getMessages();
+    }, []);
+    return (
         <div>DASHBOARD</div>
     )
 }
