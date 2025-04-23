66312b74a5c69003f5bc3e43f09948b243b354d7
Hadi <969hadiahmed@gmail.com>
2025-04-23 12:28:41 +0530

    fix: key to access token

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,13 @@ import { getToken } from "next-auth/jwt"
 
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
+    const token = await getToken({ req: request, secret: process.env.NEXT_AUTH_SECRET });
     const url = request.nextUrl;
     if (token && (
         url.pathname.startsWith("/sign-in") ||
         url.pathname.startsWith("/sign-up") ||
         url.pathname.startsWith("/verify") ||
-        url.pathname.startsWith("/")
+        url.pathname === "/"
     )) return NextResponse.redirect(new URL('/dashboard', request.url));
 
     if (!token && url.pathname.startsWith('/dashboard'))
